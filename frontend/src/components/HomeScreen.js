18e6c83d92c58f3ec366afa9c88bb5ac3004a46d
Margaux Chevreux <margaux.chevreux@orange.fr>
2020-12-30 16:03:29 +0100

    Design changes in Home Screen

--- frontend/src/components/HomeScreen.js
+++ frontend/src/components/HomeScreen.js
@@ -1,8 +1,9 @@
-// import React from "react";
-// import { StyleSheet, View, Text, Image } from "react-native";
-// import { Button, Header } from "react-native-elements";
-// import AppLoading from "expo-app-loading";
-// import { connect } from "react-redux";
+import React, { useState } from "react";
+import { Redirect } from "react-router-dom";
+import "../stylesheets/homescreen.css";
+import { connect } from "react-redux";
+import { Image } from "react-bootstrap";
+
 // import {
 //   useFonts,
 //   Montserrat_400Regular,
@@ -11,111 +12,82 @@
 //   Montserrat_700Bold,
 // } from "@expo-google-fonts/montserrat";
 
-// function HomeScreen({ navigation, username }) {
-//   const logo = require("../assets/MikeChickenRight.png");
-//   const image = require("../assets/MikeChickenLeft.png");
+function HomeScreen({ username }) {
+  //   const logo = require("../assets/MikeChickenRight.png");
+  //   const image = require("../assets/MikeChickenLeft.png");
+  //   let [fontsLoaded] = useFonts({
+  //     Montserrat_500Medium,
+  //     Montserrat_400Regular,
+  //     Montserrat_400Regular_Italic,
+  //     Montserrat_700Bold,
+  //   });
 
-//   let [fontsLoaded] = useFonts({
-//     Montserrat_500Medium,
-//     Montserrat_400Regular,
-//     Montserrat_400Regular_Italic,
-//     Montserrat_700Bold,
-//   });
-//   if (!fontsLoaded) {
-//     return <AppLoading />;
-//   } else {
-//     return (
-//       <View style={styles.container}>
-//         <Header
-//           barStyle="light-content"
-//           leftComponent={<Image source={logo} style={styles.logo} />}
-//           centerComponent={<Text style={styles.title}>InterviewCop</Text>}
-//           containerStyle={styles.topbar}
-//         />
-//         <Text style={styles.title2}> Bienvenue {username} !</Text>
-//         <Image source={image} style={styles.image} />
-//         <Text style={styles.text}> InterviewCop vous entraîne à passer des entretiens d'embauche. </Text>
-//         <Button
-//           title="Go !"
-//           titleStyle={styles.textbutton}
-//           onPress={() => {
-//             navigation.navigate("Interview");
-//           }}
-//           buttonStyle={styles.button}
-//         />
-//         <Button
-//           title="Des conseils !"
-//           titleStyle={styles.textbutton}
-//           onPress={() => {
-//             navigation.navigate("Advices");
-//           }}
-//           buttonStyle={styles.button}
-//         />
-//       </View>
-//     );
-//   }
-// }
+  const [redirectGo, setRedirectGo] = useState(false);
+  const [redirectAdvices, setRedirectAdvices] = useState(false);
 
-// function mapStateToProps(state) {
-//   return { username: state.username };
-// }
+  // déclenche la redirection vers la page Interview Screen Home si le user clique sur Go!
+  const handleClickGo = () => {
+    setRedirectGo(true);
+  };
+  if (redirectGo) {
+    return <Redirect to="/interviewscreenhome" />;
+  }
 
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     alignItems: "center",
-//     justifyContent: "flex-start",
-//     backgroundColor: "#FFFEFA",
-//   },
-//   title: {
-//     color: "#FFFEFA",
-//     fontFamily: "Montserrat_700Bold",
-//     fontSize: 22,
-//   },
-//   title2: {
-//     color: "#0773A3",
-//     fontFamily: "Montserrat_700Bold",
-//     fontSize: 26,
-//   },
-//   topbar: {
-//     backgroundColor: "#0773A3",
-//     marginBottom: 40,
-//   },
-//   text: {
-//     fontFamily: "Montserrat_500Medium",
-//     fontSize: 18,
-//     color: "#0773A3",
-//     textAlign: "center",
-//     padding: 5,
-//   },
-//   textbutton: {
-//     color: "#FFFEFA",
-//     fontFamily: "Montserrat_500Medium",
-//     fontWeight: "600",
-//     fontSize: 16,
-//     lineHeight: 29,
-//     alignItems: "center",
-//     textAlign: "center",
-//     letterSpacing: 0.75,
-//   },
-//   button: {
-//     marginTop: 20,
-//     backgroundColor: "#0773A3",
-//     borderRadius: 15,
-//     width: 140,
-//   },
-//   logo: {
-//     width: 20,
-//     height: 35,
-//     marginLeft: 70,
-//   },
-//   image: {
-//     width: 120,
-//     height: 230,
-//     marginTop: 30,
-//     marginBottom: 10,
-//     marginRight: 25,
-//   },
-// });
+  // déclenche la redirection vers la page Conseils si le user clique sur Des conseils!
+  const handleClickAdvices = () => {
+    setRedirectAdvices(true);
+  };
+  if (redirectAdvices) {
+    return <Redirect to="/advices" />;
+  }
 
-// export default connect(mapStateToProps, null)(HomeScreen);
+  return (
+    <div>
+      <nav class="navbar sticky-top navbar-light justify-content-center topbar">
+        <Image src="../images/MikeChickenRight.png" className="logo" />
+        <p className="title">InterviewCop</p>
+      </nav>
+      <div className="container-fluid home">
+        <div className="col">
+          <div className="row align-items-center justify-content-center mt-4">
+            <p className="title2">Bienvenue {username} !</p>
+          </div>
+          <div className="row align-items-center justify-content-center">
+            <Image src="../images/MikeChickenLeft.png" className="image" />
+          </div>
+          <div className="row align-items-center justify-content-center">
+            <p className="text">InterviewCop vous entraîne à passer des entretiens d'embauche</p>
+          </div>
+          <div className="row align-items-center justify-content-center">
+            <button
+              className="button"
+              onClick={() => {
+                handleClickGo();
+              }}
+              type="button"
+            >
+              Go !
+            </button>
+          </div>
+          <div className="row align-items-center justify-content-center">
+            <button
+              className="button"
+              onClick={() => {
+                handleClickAdvices();
+              }}
+              type="button"
+            >
+              Des conseils !
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function mapStateToProps(state) {
+  return { username: state.username };
+}
+
+export default connect(mapStateToProps, null)(HomeScreen);
